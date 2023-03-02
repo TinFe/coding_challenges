379aefad8e1fd02fa70c81b09c37dffb2ce8e941
TinFe <53035411+TinFe@users.noreply.github.com>
2023-03-02 16:12:04 +0700

    Create twoSum.py

---
+++ leetcode/twoSum.py
@@ -0,0 +1,66 @@
+"""
+1. Two Sum
+Easy
+43.8K
+1.4K
+Companies
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+Accepted
+9.1M
+Submissions
+18.3M
+Acceptance Rate
+49.5%
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        table = dict()
+        x=0
+        
+        while x < len(nums):
+        
+            table[nums[x]] =x
+            x+=1
+        
+        index = 0
+        for i in nums:
+            search = target - i
+            if search in table and index != table[search]:
+                return (index, table[search])
+            index += 1
