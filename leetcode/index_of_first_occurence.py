fc45eb45fc8f8bf616890add034af56d3c87e1d2
TinFe <53035411+TinFe@users.noreply.github.com>
2023-03-08 08:42:00 +0700

    Create index_of_first_occurence.py

---
+++ leetcode/index_of_first_occurence.py
@@ -0,0 +1,31 @@
+"""
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+"""
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        slice_size = len(needle)
+
+        for c in enumerate(haystack):
+            if c[0] + slice_size <= len(haystack):
+                if haystack[c[0]:(c[0]+slice_size)] == needle:
+                    return c[0]
+        return -1
